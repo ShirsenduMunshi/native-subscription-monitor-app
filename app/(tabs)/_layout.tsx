9cc1a1c0b73df2b1fc1ccf0788e236b84d7d0797
Shirsendu Munshi <shirsendumunshi@gmail.com>
2026-07-05 15:09:21 +0530

    Update app/(tabs)/_layout.tsx
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -8,17 +8,19 @@ import { View } from 'react-native';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import {colors, components, spacing} from '@/constents/theme';
 
+const TabIcon = ({ focused, icon }: TabIconProps) => {
+    return (
+        <View className='tabs-icon'>
+            <View className={clsx('tabs-pill', focused && "tabs-active")}>
+                <Image source={icon} resizeMode='contain' className='tabs-glyph' />
+            </View>
+        </View>
+    )
+}
+
 const TabLayout = () => {
     const insets = useSafeAreaInsets()
-    const TabIcon = ({ focused, icon }: TabIconProps) => {
-        return (
-            <View className='tabs-icon'>
-                <View className={clsx('tabs-pill', focused && "tabs-active")}>
-                    <Image source={icon} resizeMode='contain' className='tabs-glyph' />
-                </View>
-            </View>
-        )
-    }
+    return (
     return (
         <Tabs 
             screenOptions={
